ea9355277ab3d330ecd84ee2390213f94724bd60
ffc2107 <ffc2107@outlook.com>
2025-05-03 21:22:29 -0300

    terceiro projeto

---
+++ index.js
@@ -0,0 +1,41 @@
+// Criei uma classe
+class Heroi {
+    constructor(nome, idade, tipo) {
+      this.nome = nome;
+      this.idade = idade;
+      this.tipo = tipo;
+    }
+    
+    //Criei um método atacar e dentro utilizei um switch case com os tipos e quais seriam os ataques usados.
+    atacar() {
+      let ataque = "";
+      switch (this.tipo) {
+        case "mago":
+          ataque = "magia";
+          break;
+        case "guerreiro":
+          ataque = "espada";
+          break;
+        case "monge":
+          ataque = "artes marciais";
+          break;
+        case "ninja":
+          ataque = "shuriken";
+          break;
+        default:
+          ataque = "ataque inexistente";
+      }
+      
+      //Saída
+      console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+  }
+  
+  // Criei um objeto 
+  const mago = new Heroi("Hermes", 49, "monge");
+  // Saída
+  mago.atacar(); 
+  
+  
+
+  
